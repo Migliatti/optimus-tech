fa0c6cfb5c03379db6c1efbaa5923222f4378c9f
Gabriel Migliatti <101683285+Migliatti@users.noreply.github.com>
2023-01-24 20:24:58 -0300

    Cabeçalho acabado

--- optimustech/src/components/Header/index.js
+++ optimustech/src/components/Header/index.js
@@ -1,11 +1,34 @@
-import './Header.css';
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <header>
-            <h1> Acho que ta funfa</h1>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="/img/logo.svg" alt="Logo da Optimus Tech" />
+        <h1>Optimus Tech</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <button>Home</button>
+          </li>
+          <li>
+            <button>Produtos</button>
+          </li>
+          <li>
+            <button>Recursos</button>
+          </li>
+          <li>
+            <button>Sobre nós</button>
+          </li>
+        </ul>
+      </nav>
+      <div className="create-account">
+        <button className="login">Entrar</button>
+        <button className="sing">Cadastrar</button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
